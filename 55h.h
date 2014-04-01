fe9cedfff4814f32c51f3d3ee5d29f388f203a58
skyballoon <balonangkasa@gmail.com>
2014-04-01 15:46:22 +0700

    Create 55h.h
    run daily with crontab
    0 7,13,19,1 * * * /usr/bin/python /u01/app/oracle/backups/scripts/pool.bin -o h >>/mnt/bcks/rman/logs/55_`date +\%Y\%m\%d\%H\%M`.hog

---
+++ 55h.h
@@ -0,0 +1,29 @@
+#!/bin/bash
+dateformat=$(date +%d%m%Y_%H%M%S)
+
+date
+c_begin_time_sec=`date +%s`
+
+su -c "
+. /home/oracle/.db_profile
+export NLS_DATE_FORMAT='YYYY/MM/DD HH24:MI:SS'
+rman target=/ log=/mnt/bcks/rman/archivelog/55/logs/arc_${dateformat}.log << EOF
+SET ECHO ON;
+RUN {
+  SHOW ALL;
+  CONFIGURE CONTROLFILE AUTOBACKUP ON;
+  CONFIGURE CONTROLFILE AUTOBACKUP FORMAT FOR DEVICE TYPE DISK TO '/mnt/bcks/rman/controlfile/55/55ctl_%F';
+
+  BACKUP ARCHIVELOG ALL NOT BACKED UP 1 TIMES TAG 'ARC-HOURLY${dateformat}';
+  DELETE NOPROMPT ARCHIVELOG ALL COMPLETED BEFORE 'SYSDATE-8/24' BACKED UP 1 TIMES TO DISK;
+
+  DELETE NOPROMPT BACKUP OF ARCHIVELOG ALL COMPLETED BEFORE 'SYSDATE-3';
+}
+EXIT;
+EOF
+" oracle
+
+date
+c_end_time_sec=`date +%s`
+v_total_execution_time_sec=`expr ${c_end_time_sec} - ${c_begin_time_sec}`
+echo "Script execution time is $v_total_execution_time_sec seconds"
